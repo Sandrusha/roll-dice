702eee77ce0115dd99e4d44066b2b90174746cb9
Sandra P <putere.sandra@gmail.com>
2019-12-08 09:18:02 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+var dice = ["dice-"];
+var maxDice = 6;
+
+document.addEventListener("DOMContentLoaded", onLoad);
+
+function rollDice() {
+    var rndNumber = Math.floor(Math.random() * maxDice + 1);
+
+    return rndNumber;
+}
+
+function onLoad() {
+    var leftDiceDiv = document.getElementById("leftDiceDiv");
+    var diceSideLeft = leftDiceDiv.getElementsByClassName("dice");
+    var diceInfoLeft = rollDice();
+    diceSideLeft[0].style.backgroundImage = `url(images/dice-${diceInfoLeft}.svg)`;
+
+    var rightDiceDiv = document.getElementById("rightDiceDiv");
+    var diceSideRight = rightDiceDiv.getElementsByClassName("dice");
+    var diceInfoRight = rollDice();
+    diceSideRight[0].style.backgroundImage = `url(images/dice-${diceInfoRight}.svg)`;
+ 
+
+    winOrLose(diceInfoLeft, diceInfoRight);
+}
+
+function winOrLose(diceInfoLeft, diceInfoRight) {
+    var resolution = document.getElementById("resolution");
+
+    if (diceInfoLeft < diceInfoRight) {
+        resolution.innerHTML = "You Lose!";
+    } else if (diceInfoLeft === diceInfoRight) {
+        resolution.innerHTML = "Roll Again!"
+    } else {
+        resolution.innerHTML = "You Win!"
+    }
+}
